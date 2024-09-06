642a3de03dedcb42b2b827cab2a63ce1481015ed
Ajith-NM <ajith.n.m5603@gmail.com>
2024-09-06 15:46:58 +0530

    add jwt error msg

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 export const userAuthentication = (req, res, next) => {
   let token = req.cookies.token;
   if (token == undefined) {
-    res.status(401).send("no token provided");
+    res.status(401).json({ status: false,response: "failed"});
     return;
   }
 
   jwt.verify(token, process.env.secret, (err, decoded) => {
     if (err) {
       console.log(err);
-      res.status(500).send("Authentication failed");
+      res.status(400).json({ status: false,response: "failed"});
     } else {
       req.userId = decoded.user_Id;
       next();
